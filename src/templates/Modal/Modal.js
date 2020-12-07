cd3de5bd15308a75a442ae88fa1a05b271aac3d6
Maximusvin <Maximusvin@gmail.com>
2020-12-07 17:22:25 +0200

    added Modal template component

---
+++ src/templates/Modal/Modal.js
@@ -0,0 +1,38 @@
+import { Component } from 'react';
+import { createPortal } from 'react-dom';
+import s from './Modal.module.css';
+
+const modalRoot = document.querySelector('#modal-root');
+
+class Modal extends Component {
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = e => {
+    if (e.code === 'Escape') {
+      this.props.onCloseModal();
+    }
+  };
+
+  handleBackdropClick = e => {
+    if (e.target === e.currentTarget) {
+      this.props.onCloseModal();
+    }
+  };
+
+  render() {
+    return createPortal(
+      <div className={s.backdrop} onClick={this.handleBackdropClick}>
+        <div className={s.modal}>{this.props.children}</div>
+      </div>,
+      modalRoot,
+    );
+  }
+}
+
+export default Modal;
