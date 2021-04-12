726c5b6f1a143377a3b70dbd3a88cc3a21519970
Maxim Kozlov <Maximusvin@gmail.com>
2021-04-12 22:07:06 +0300

    Changed the structure of the files. Added absolute paths

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+// import { Route } from 'react-router-dom';
+// import { Header, Footer, Main, Home, Questions } from 'components';
+// import { AuthPage, ContactsPage, UseFulPage } from 'views';
+
+function App() {
+  return (
+    <>
+      <h1>Home Work #2.2</h1>
+      <div>Hello</div>
+    </>
+  );
+}
+
+export default App;
--- src/components/Layout/Layout.js
+++
@@ -1,7 +0,0 @@
-import s from './Layout.module.css';
-
-const Layout = ({ children }) => {
-  return <div className={s.container}>{children}</div>;
-};
-
-export default Layout;
